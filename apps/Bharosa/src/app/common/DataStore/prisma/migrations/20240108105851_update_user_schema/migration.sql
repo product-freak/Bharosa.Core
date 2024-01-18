1430d19c147083b31dd1e3c990233961d3a8bc1c
Akhila Gopu <gopuakhila1995@gmail.com>
2024-01-18 18:29:08 +0530

    remove unwanted migrations

--- apps/Bharosa/src/app/common/DataStore/prisma/migrations/20240108105851_update_user_schema/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `phone_number` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "email" DROP NOT NULL,
-ALTER COLUMN "phone_number" SET NOT NULL;
